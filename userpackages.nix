8662dc4c1afb2cb49cbb34d2c6f53fc79bcaee33
Nathan van Doorn <nathan@myrtlesoftware.com>
2019-07-02 14:12:10 +0100

    Add userpackages.nix in its current state

---
+++ userpackages.nix
@@ -0,0 +1,50 @@
+self: super:
+
+{
+  userPackages = super.userPackages or {} // {
+    inherit (self)
+      cabal-install
+      cabal2nix
+      ed
+      emacs
+      ghc
+      gitFull
+      graphviz
+      jq
+      libreoffice
+      meld
+      pandoc
+      nix-index
+      ripgrep
+      rustup
+      shellcheck
+      tmux
+      tree
+      watchexec;
+    inherit (self.haskellPackages)
+      ghcid
+      glirc;
+    inherit (self.texlive.combined) scheme-full;
+    neovim = self.neovim.override {
+      vimAlias = true;
+      viAlias = true;
+      configure = {
+        customRC = ''
+          set mouse=a
+          set number
+          set expandtab
+          colorscheme industry
+        '';
+        packages.myNvimPackages = with self.vimPlugins; {
+          start = [ elm-vim vim-nix vim-surround ctrlp vim-toml ];
+          opt = [];
+        };
+      };
+    };
+    nix-rebuild = super.writeScriptBin "nix-rebuild"
+      ''
+        #!${super.stdenv.shell}
+	exec nix-env -f '<nixpkgs>' -r -iA userPackages
+      '';
+  };
+}
